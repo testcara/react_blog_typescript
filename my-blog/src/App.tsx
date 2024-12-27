316c96c9424c29ba2e2803cb30caf5ded4beb55d
Cara Wang <wlin@redhat.com>
2024-12-27 15:29:08 +0800

    inital the project
    just including the package & tsconfig to ensure
    npm start the server well.

---
+++ my-blog/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Button } from '@patternfly/react-core';
+
+function App() {
+  return (
+    <><Button>测试</Button></>
+  );
+}
+
+export default App;
