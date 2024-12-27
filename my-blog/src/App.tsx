c148f8615c220bc85d0765786ede123fd60e87ac
Cara Wang <wlin@redhat.com>
2024-12-27 17:35:27 +0800

    add prettier and eslint

--- my-blog/src/App.tsx
+++ my-blog/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Button } from '@patternfly/react-core';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
 function App() {
   return (
-    <><Button>测试</Button></>
+    <>
+    <Header></Header>
+    <div className='inner'>
+    </div>
+    <Footer></Footer>
+    </>
   );
 }
 
